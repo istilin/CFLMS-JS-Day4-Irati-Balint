4970f4a0d4907c2194b5e025b3962cccd74ecc8e
Irati Larreina <irati_6@hotmail.com>
2020-05-28 10:01:44 +0200

    Started exercise 1 basic

---
+++ Basic/Exercise1/script.js
@@ -0,0 +1,7 @@
+result_firstname = document.getElementById("firstname")
+result_lastname = document.getElementById("lastname")
+result_age = document.getElementById("age")
+
+document.getElementById("theform").addEventListener("submit", (event) => {
+	console.log("hello")
+})
