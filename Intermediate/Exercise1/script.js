782b7dca0f99002ec7d744a2e221a75d184b0e7e
Balint Bakos <balint.bb@gmail.com>
2020-05-28 11:01:41 +0200

    adds intermediate ex

--- Intermediate/Exercise1/script.js
+++ Intermediate/Exercise1/script.js
@@ -1,4 +1,6 @@
-class Hotel{
+/* Creating our Class (Blueprint) */
+
+class Hotel {
 	hotelName = ""
 	numberOfNights = Math.floor(Math.random()*10) + 1
 	address = ""
@@ -10,13 +12,14 @@ class Hotel{
 		this.image = image
 	}
 
+	/* Method will return some html depending on the condition */
 	showAvailability(numNights){
 
 		if (numNights > this.numberOfNights){
 			return `<p>Sorry, no available nights for ${this.hotelName}.</p>`
 
 		} else{
-			return `<button> I'll reserve </button>`
+			return `<br><button> I'll reserve </button>`
 
 		}
 
@@ -24,12 +27,43 @@ class Hotel{
 
 }
 
+/* We create our hotel instances */
+
 const hotel1 = new Hotel("Hilton", "Blabla 10", "https://r-cf.bstatic.com/images/hotel/max1024x768/475/47526446.jpg")
 const hotel2 = new Hotel("Hotel somethingsomething", "Blabla 20", "https://r-cf.bstatic.com/images/hotel/max1024x768/475/47526446.jpg")
 const hotel3 = new Hotel("Hostel thingy", "Blabla 30", "https://r-cf.bstatic.com/images/hotel/max1024x768/475/47526446.jpg")
 
-console.log(hotel1)
-console.log(hotel2)
-console.log(hotel3)
+/* We put the hotel instances into an array to have the opportunity to render them */
 
-console.log(hotel1.showAvailability(4))
+const hotels = [hotel1, hotel2, hotel3]
+
+
+/* Rendering function */
+
+const generateDOM = () => {
+	const hotelsDiv = document.getElementById('hotels')
+
+	hotels.map((hotel) => {
+		/* Creating the html elements */
+		const nameP = document.createElement('p')
+		const addressP = document.createElement('p')
+		const img = document.createElement('img')
+		const availableOrNot = hotel.showAvailability(Math.floor(Math.random() * 5))
+
+		/* Setting up the html elements */
+		nameP.innerText = hotel.hotelName
+		addressP.innerText = hotel.address
+		img.setAttribute('src', hotel.image)
+
+		/* Appending the html elements to our hotels div */
+		hotelsDiv.appendChild(nameP)
+		hotelsDiv.appendChild(addressP)
+		hotelsDiv.appendChild(img)
+		hotelsDiv.innerHTML += availableOrNot
+
+
+	})
+}
+
+/* Calling the render function */
+generateDOM()
