a5057c68eecf257052f0be6b9e9774f0795ec9bc
Balint Bakos <balint.bb@gmail.com>
2020-05-28 10:20:31 +0200

    adds exercise 1-2

--- Basic/Exercise1/script.js
+++ Basic/Exercise1/script.js
@@ -1,7 +1,43 @@
-result_firstname = document.getElementById("firstname")
-result_lastname = document.getElementById("lastname")
-result_age = document.getElementById("age")
+/* Adding event listener for the submit event */
 
 document.getElementById("theform").addEventListener("submit", (event) => {
+	/* Overwriting the default behavior of submit event (refresh) */
+	event.preventDefault()
 	console.log("hello")
+	/* Getting the value of out of our input field */
+	result_firstname = document.getElementById("firstname").value
+	console.log(result_firstname)
+	result_lastname = document.getElementById("lastname").value
+	console.log(result_lastname)
+	result_age = document.getElementById("age").value
+	console.log(result_age)
+
+	/* Calling the function with the data from the input field to generate the dom elements */
+	generateDOM(result_firstname, result_lastname, result_age)
+
+
 })
+
+/* Generates the DOM elements */
+const generateDOM = (firstname, lastname, age) => {
+	/* Prepering out results div in variables */
+	const resultFirst = document.getElementById('resultfirstname')
+	const resultLast = document.getElementById('resultlastname')
+	const resultAge = document.getElementById('resultage')
+	const wrapper = document.getElementById('wrapper')
+
+	const profession = document.getElementById('profession').value
+	/* Using the ternary operator instead of writing an if else statement */
+
+	const divColor = profession === 'it' ? 'purple' : 'yellow'
+	wrapper.style.backgroundColor = divColor
+
+	/* Using the ternary operator instead of writing an if else statement */
+	const color = firstname.length > 5 ? 'green' : 'red'
+	resultFirst.style.color = color
+
+	resultFirst.innerHTML = firstname
+	resultLast.innerHTML = lastname
+	resultAge.innerHTML = age
+
+}
